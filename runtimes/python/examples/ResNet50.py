7dd3de118f9e0f51edf789e029a908da12ab464b
jstamel <jgstamelos@gmail.com>
2020-06-16 13:33:09 +0300

    Update ResNet50.py

--- runtimes/python/examples/ResNet50.py
+++ runtimes/python/examples/ResNet50.py
@@ -9,38 +9,47 @@ import tempfile
 from contextlib import contextmanager
 from inaccel.keras.applications.resnet50 import decode_predictions, ResNet50
 from inaccel.keras.preprocessing.image import load_img
+from werkzeug.exceptions import BadRequest
 
 @contextmanager
 def fetch_img(img_url):
-	headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
-	img = requests.get(img_url, headers=headers, stream=True)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
+    img = requests.get(img_url, headers=headers, stream=True)
 
-	content, _type = img.headers['Content-Type'].split('/')
-	if content == 'image':
-		ext = mimetypes.guess_extension(img.headers['Content-Type'])
-		if not ext:
-			ext = _type
-	else:
-		raise requests.exceptions.InvalidURL("Given URL is not of Content-Type image")
+    content, _type = img.headers['Content-Type'].split('/')
+    if content == 'image':
+        ext = mimetypes.guess_extension(img.headers['Content-Type'])
+        if not ext:
+            ext = _type
+    else:
+        raise requests.exceptions.InvalidURL("Given URL is not of Content-Type image")
 
-	try:
-		with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as img_file:
-			shutil.copyfileobj(img.raw, img_file)
-		yield img_file.name
-	except (FileNotFoundError, FileExistsError, PermissionError, BaseException):
-		raise
-	else:
-		os.remove(img_file.name)
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as img_file:
+            shutil.copyfileobj(img.raw, img_file)
+        yield img_file.name
+    except (FileNotFoundError, FileExistsError, PermissionError, BaseException):
+        raise
+    else:
+        os.remove(img_file.name)
 
 def preprocess_input(img_url):
-	with fetch_img(img_url) as img_file:
-		return np.expand_dims((load_img(img_file, target_size=(224, 224))), axis=0)
+    with fetch_img(img_url) as img_file:
+        return np.expand_dims((load_img(img_file, target_size=(224, 224))), axis=0)
 
-def predict(event, context):
-	if event['data']:
-		model = ResNet50(weights='imagenet')
+def predict(request):
+    try:
+        data = request.get_json(force=True)
 
-		images = np.vstack(list(map(preprocess_input, json.loads(event['data']))))
+        if data:
+            model = ResNet50(weights='imagenet')
 
-		preds = model.predict(images)
-		return str(decode_predictions(preds, top=1))
+            images = np.vstack(list(map(preprocess_input, data)))
+
+            preds = model.predict(images)
+
+            return str(decode_predictions(preds, top=1))
+        else:
+            return ""
+    except BadRequest:
+        return "No valid input JSON data!"
